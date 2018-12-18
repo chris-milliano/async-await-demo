a31879b1ef9288ed42d61246a426185c3b866593
Chris Milliano <christopher_milliano@intuit.com>
2018-12-18 13:18:57 -0800

    add exampes of other async types

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/color/:hex?/:secs?', (req, res) => {
     const hexCode = req.params.hex || '00ff00';
     const secs = req.params.secs || 3;
@@ -9,8 +10,37 @@ app.get('/color/:hex?/:secs?', (req, res) => {
     }, (secs * 1000));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+
+app.get('/callback', (req, res) => {
+    res.sendFile(__dirname + '/views/callback.html');
+});
+
+app.get('/promise', (req, res) => {
+    res.sendFile(__dirname + '/views/promise.html');
+});
+
+app.get('/await', (req, res) => {
+    res.sendFile(__dirname + '/views/await.html');
+});
+
+app.get('/callback/crazy', (req, res) => {
+    res.sendFile(__dirname + '/views/callback-crazy.html');
+});
+
+app.get('/promise/crazy', (req, res) => {
+    res.sendFile(__dirname + '/views/promise-crazy.html');
+});
+
+app.get('/await/crazy', (req, res) => {
+    res.sendFile(__dirname + '/views/await-crazy.html');
+});
+
+app.get('/await/warning', (req, res) => {
+    res.sendFile(__dirname + '/views/await-warning.html');
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send();
 });
 
 let PORT = 3000;
